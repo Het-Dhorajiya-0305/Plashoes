70ddad9255e679bf1846717c7e37ca9ee5222a4b
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2025-07-16 14:55:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 6001, () => {
+        app.listen(process.env.PORT || 3001, () => {
             console.log(`server is running on port ${process.env.PORT || 3001}`);
         })
     })
